38655eaddcb668d3e21041928c0ccc8890bfaffb
Michael Kir <michael.kir@icloud.com>
2024-09-24 15:31:24 +0100

    Publish deploy 2024-09-24 15:31

---
+++ js/googleAnalytics.js
@@ -0,0 +1,16 @@
+//
+//  googleAnalytics.js
+//  Flavournomics
+//
+//  Created by Michael Kir on 23/09/2024.
+//
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RBZYEYQ85V"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RBZYEYQ85V');
+</script>
